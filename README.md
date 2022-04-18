7125aec0b0609561a015931e1e39ff5522a516b3
arfat132 <arfatbegum90@gmail.com>
2022-04-18 19:02:38 +0300

    blogs edited

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # Core Realtor
 
-Live Site Link: [Core Realtor](https://github.com/facebook/create-react-app).
+Live Site Link: [Core Realtor](https://core-realtor-auth.web.app/).
 
 ## Features
 
 * This is a realtor independent website.
-* In this website a realtor provide 3 services.
+* In this website a realtor provides 3 services.
 * In this website have sign up and login system.
 * In this website has a private routh page.
-* In this website have a blog page and a about me page.
+* In this website have a blog page, a about me page and a not found page.
 
 ### Used Technologies
 
 * React.js
 * React Router
-* Tailwind
+* Tailwind css
 * TailBlocks
 * React icons
 * Firebase
--- src/components/IconsGroup/IconGroup.js
+++ src/components/IconsGroup/IconGroup.js
@@ -2,34 +2,30 @@ import React from 'react';
 import { BiBuildingHouse } from "@react-icons/all-files/bi/BiBuildingHouse";
 import { GiFamilyHouse } from "@react-icons/all-files/gi/GiFamilyHouse";
 import { BsFillHouseFill } from "@react-icons/all-files/bs/BsFillHouseFill";
-import { GiSpookyHouse } from "@react-icons/all-files/gi/GiSpookyHouse";
 import { GiGreenhouse } from "@react-icons/all-files/gi/GiGreenhouse";
 import { GiLighthouse } from "@react-icons/all-files/gi/GiLighthouse";
 const IconGroup = () => {
     return (
         <div className='text-center font-bold px-36 justify-between text-blue-900 flex flex-wrap' >
-            <div className='sm:mb-8 lg:mb-0'>
+            <div className='mb-4 bg-blue-100 py-8 px-12 rounded-tr-3xl rounded-bl-3xl'>
                 <BsFillHouseFill className='text-9xl mb-2'> </BsFillHouseFill>
                 <h3>Single House</h3>
             </div>
-            <div className='sm:pb-8 lg:pb-0'>
-                <BiBuildingHouse className='text-9xl mb-2'> </BiBuildingHouse>
+            <div className='mb-4 bg-blue-100 py-8 px-12 rounded-tr-3xl rounded-bl-3xl'>
+                <BiBuildingHouse className='text-9xl mb-2 '> </BiBuildingHouse>
                 <h3>Single House</h3>
             </div>
-            <div className='sm:mb-8 lg:mb-0'>
+            <div className='mb-4 bg-blue-100 py-8 px-12 rounded-tr-3xl rounded-bl-3xl'>
                 <GiFamilyHouse className='text-9xl mb-2' >  </GiFamilyHouse>
                 <h3>Family House</h3>
             </div>
-            <div className='sm:mb-8 lg:mb-0'>
+            <div className='mb-4 bg-blue-100 py-8 px-12 rounded-tr-3xl rounded-bl-3xl'>
                 <GiLighthouse className='text-9xl mb-2'></GiLighthouse>
                 <h3>Light House</h3>
             </div>
-            <div className='sm:mb-8 lg:mb-0'>
-                <GiSpookyHouse className='text-9xl mb-2'></GiSpookyHouse>
-                <h3>Spooky House</h3>
-            </div>
-            <div className=''>
-                <GiGreenhouse className='text-9xl mb-2'></GiGreenhouse>
+            
+            <div className='bg-blue-100 py-8 px-12 mb-4 rounded-tr-3xl rounded-bl-3xl   q'>
+                <GiGreenhouse className='text-9xl mb-2 '></GiGreenhouse>
                 <h3>Green House</h3>
             </div>
 
